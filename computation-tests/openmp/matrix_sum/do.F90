1ba65cda3f80bcee39a6a4808cb54d3bdaa609ff
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-02-18 10:54:44 -0600

    using new timing method

--- computation-tests/openmp/matrix_sum/do.F90
+++ computation-tests/openmp/matrix_sum/do.F90
@@ -1,21 +1,15 @@
 program matvec_doloop
+  use benchmark_tools
   implicit none
   double precision, dimension(:,:), allocatable :: A,B,C
-  integer :: m, n, i, j, sum, max_n, step
+  integer :: m, n, i, j, sum, max_n, step, fint
   integer(kind=8) :: count_fin, count_init, count_rate
   double precision :: time_fin, time_init, count_rate_d
-  character(:), allocatable :: fname, arg1
+  character(:), allocatable :: arg1, prob
   logical :: fexists
-  fname = "output_matsum.txt"
-  inquire(file=fname, exist=fexists)
-  open(2, file=fname,action='write',position='append')
-  if (fexists .eqv. .false.) then
-    ! open(2, file=fname,action='write')
-    write(2,*) "n,    time,     method"
-  else
 
-  end if
-
+  prob = "matrix_sum"
+  call open_report(prob, fint)
 
   n = 64
   max_n = 20000
@@ -47,9 +41,8 @@ program matvec_doloop
   time_init = count_init / count_rate_d
   time_fin  = count_fin  / count_rate_d
   ! print *, "column major time:", time_fin - time_init
-  write(2,fmt="(I8,A2)", advance="no") n, ", "
-  write(2,fmt="(F20.6,A2)", advance="no") time_fin - time_init, ", "
-  write(2,fmt="(A2)") "do"
+  call report(n,time_fin - time_init, "do", fint)
+
 
   deallocate(a,b,c)
   n = n * step
