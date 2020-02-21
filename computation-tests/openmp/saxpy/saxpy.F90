8419a4833be7b34470c57136e9b01410bfa5bff2
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-02-21 08:35:06 +0000

    adding

---
+++ computation-tests/openmp/saxpy/saxpy.F90
@@ -0,0 +1,39 @@
+program saxpy_do
+  use benchmark_tools
+  implicit none
+
+
+  ! prob = "saxpy"
+  ! call open_report(prob, fint)
+
+  ! time = compute_time(count_rate, count_init, count_fin)
+  ! call report(n, time, "do", fint)
+
+  subroutine saxpy_array_syntax(X,Y,a,n)
+    real, intent(IN) :: X(n), a
+    real, intent(INOUT) :: Y(n)
+    integer, intent(IN) :: n
+    integer :: i
+    Y = a*X + Y
+  end subroutine saxpy_array_syntax
+
+  subroutine saxpy_do(X,Y,a,n)
+    real, intent(IN) :: X(n), a
+    real, intent(INOUT) :: Y(n)
+    integer, intent(IN) :: n
+    integer :: i
+    do i=1,n
+       Y(i) = a*X(i) + Y(i)
+    end do
+  end subroutine saxpy_do
+
+  subroutine saxpy_do_concurrent(X,Y,a,n)
+    real, intent(IN) :: X(n), a
+    real, intent(INOUT) :: Y(n)
+    integer, intent(IN) :: n
+    integer :: i
+    do concurrent (i=1:n)
+       Y(i) = a*X(i) + Y(i)
+    end do
+  end subroutine saxpy_do_concurrent
+end module saxpy
