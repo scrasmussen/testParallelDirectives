e8c0179d01020a7348f5bbe451e206616b8a7ec3
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-02-27 03:54:49 -0600

    for some reason this works

--- computation-tests/openmp/matrix_sum/ompTargetDataless.F90
+++ computation-tests/openmp/matrix_sum/ompTargetDataless.F90
@@ -1,7 +1,7 @@
 program matsum_doloop
   use benchmark_tools
   implicit none
-  double precision, dimension(:,:), allocatable :: A,B,C
+  double precision, dimension(:,:), allocatable :: A_output,B_input,C_input
   integer :: m, n, i, j, sum, max_n, step, fint
   integer(kind=8) :: count_fin, count_init, count_rate
   double precision :: time
@@ -18,33 +18,33 @@ program matsum_doloop
   do while (n .le. max_n)
 
   m = n
-  allocate(a(n,m))
-  allocate(b(n,m))
-  allocate(c(n,m))
+  allocate(a_output(n,m))
+  allocate(b_input(n,m))
+  allocate(c_input(n,m))
 
   call random_init(.true.,.false.)
-  call random_number(b)
-  call random_number(c)
-  a = 0
+  call random_number(b_input)
+  call random_number(c_input)
+  a_output = 0
 
-  !$omp target enter data map(to:B,C)
+  !$omp target enter data map(to:B_input,C_input)
 
   call system_clock(count_init, count_rate)
   ! do loop
   !$omp target teams distribute parallel do collapse(2)
   do j=1,n
     do i=1,m
-      A(i,j) = B(i,j) + C(i,j)
+      A_output(i,j) = B_input(i,j) + C_input(i,j)
     end do
   end do
   !$omp end target teams distribute parallel do
   call system_clock(count_fin)
-  !$omp target exit data map(from:A)
+  !$omp target exit data map(from:A_output)
 
   time = compute_time(count_rate, count_init, count_fin)
   call report(n, time, ompTargetDataless_m, omp_api, matsum_p, fint)
 
-  deallocate(a,b,c)
+  deallocate(a_output,b_input,c_input)
   n = n * step
   end do
 
