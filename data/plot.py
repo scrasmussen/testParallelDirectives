64b874b5efe45dea9f7b144a4e939bf46ca5b1fd
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-02-28 12:19:31 +0000

    updating paths

--- data/plot.py
+++ data/plot.py
@@ -2,12 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# data = pd.read_csv('cray-general-no-opt.txt', sep=',')
-data = pd.read_csv('tmp-cray2.txt', sep=',')
-fig, ax = plt.subplots()
+file='official/output.txt'
+data = pd.read_csv(file, sep=',')
 
-for name, group in data.groupby('method'):
-    group.plot(x='n', y='time', kind='line', ax=ax, label=name)
 
-plt.show()
+# fig, ax = plt.subplots()
+# for name, group in data.groupby('method'):
+#     group.plot(x='n', y='time', kind='line', ax=ax, label=name)
+# plt.show()
+
+
+
 # print(data)
