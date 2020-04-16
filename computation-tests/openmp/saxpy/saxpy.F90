ef7fafb0495ddc810e603eb5566ec72765e25eb0
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-04-16 10:12:25 +0100

    completed?

--- computation-tests/openmp/saxpy/saxpy.F90
+++ computation-tests/openmp/saxpy/saxpy.F90
@@ -1,39 +1,107 @@
-program saxpy_do
+program matsum_do
   use benchmark_tools
   implicit none
+  real, dimension(:), allocatable :: z,x,y
+  real :: alpha
+  integer :: n, i, j, max_n, step, fint, message
+  integer(kind=8) :: count_fin, count_init, count_rate
+  double precision :: time
+  logical :: fexists
+
+  call open_report(fint)
+
+  n = 64
+  max_n = 20000
+  ! max_n = 500
+  step = 2
+  ! allocate(a(n),b(n,m),c(m))
+  do while (n .le. max_n)
+
+  allocate(z(n))
+  allocate(x(n))
+  allocate(y(n))
+
+  call random_init(.true.,.false.)
+  call random_number(x)
+  call random_number(y)
+  call random_number(alpha)
+  z = 0
+
+#ifdef OMPTARGETDATALESS
+  !$omp target enter data map(to:alpha,to:x,to:y)
+#endif
+
+  call system_clock(count_init, count_rate)
+
+#ifdef ARRAYSYNTAX
+  z = alpha * x + y
+#endif
+
+#ifdef DO
+  do i=1,n
+     z(i) = alpha * x(i) + y(i)
+  end do
+#endif
+
+#ifdef DOCONCURRENT1
+  do concurrent (i=1:n)
+     z(i) = alpha * x(i) + y(i)
+  end do
+#endif
+
+#ifdef OMPPARALLELDO
+  !$omp parallel do
+  do i=1,n
+     z(i) = alpha * x(i) + y(i)
+  end do
+  !$omp end parallel do
+#endif
+
+#ifdef OMPPARALLELLOOP
+  !$omp parallel loop
+  do i=1,n
+     z(i) = alpha * x(i) + y(i)
+  end do
+  !$omp end parallel loop
+#endif
+
+#ifdef OMPWORKSHARE
+  !$omp parallel workshare
+  do i=1,n
+     z = alpha * x + y
+  end do
+  !$omp end parallel workshare
+#endif
+
+#ifdef OMPTARGET
+  !$omp target teams distribute parallel do collapse(1)
+  do i=1,n
+     z(i) = alpha * x(i) + y(i)
+  end do
+  !$omp end target teams distribute parallel do
+#endif
+
+#ifdef OMPTARGETDATALESS
+  !$omp target teams distribute parallel do collapse(1)
+  do i=1,n
+     z(i) = alpha * x(i) + y(i)
+  end do
+  !$omp end target teams distribute parallel do
+#endif
+
+! ---- Stop the timer ----
+  call system_clock(count_fin)
+
+#ifdef OMPTARGETDATALESS
+  !$omp target exit data map(from:z)
+#endif
+
+  time = compute_time(count_rate, count_init, count_fin)
+  call report(n, time, MESSAGE_m, API_api, PROBLEM_p, fint)
 
 
-  ! prob = "saxpy"
-  ! call open_report(prob, fint)
+  deallocate(z,x,y)
+  n = n * step
+  end do
 
-  ! time = compute_time(count_rate, count_init, count_fin)
-  ! call report(n, time, "do", fint)
-
-  subroutine saxpy_array_syntax(X,Y,a,n)
-    real, intent(IN) :: X(n), a
-    real, intent(INOUT) :: Y(n)
-    integer, intent(IN) :: n
-    integer :: i
-    Y = a*X + Y
-  end subroutine saxpy_array_syntax
-
-  subroutine saxpy_do(X,Y,a,n)
-    real, intent(IN) :: X(n), a
-    real, intent(INOUT) :: Y(n)
-    integer, intent(IN) :: n
-    integer :: i
-    do i=1,n
-       Y(i) = a*X(i) + Y(i)
-    end do
-  end subroutine saxpy_do
-
-  subroutine saxpy_do_concurrent(X,Y,a,n)
-    real, intent(IN) :: X(n), a
-    real, intent(INOUT) :: Y(n)
-    integer, intent(IN) :: n
-    integer :: i
-    do concurrent (i=1:n)
-       Y(i) = a*X(i) + Y(i)
-    end do
-  end subroutine saxpy_do_concurrent
-end module saxpy
+end program matsum_do
