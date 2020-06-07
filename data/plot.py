432f5b0f785e381be98a7f66e81b7a466fd7ca76
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-06-07 13:37:29 +0100

    adding ways to plot

--- data/plot.py
+++ data/plot.py
@@ -41,6 +41,17 @@ class color(Enum):
     ompTargetArraySyntax_m = auto()
     ompWorkshare_m = auto()
 
+class problem_c(Enum):
+    matsum  = 0
+    saxpy   = auto()
+    matmult = auto()
+
+class compiler_c(Enum):
+    cray  = 0
+    gnu   = auto()
+    intel = auto()
+    pgi   = auto()
+
 color_map = plt.get_cmap("tab20").colors
 
 compiler_e=['cray_c','gnu_c','intel_c','pgi_c']
@@ -48,6 +59,10 @@ problem_e = ['matsum','saxpy','matmult']
 name_e = ['arraySyntax', 'do', 'doConcurrent1', 'doConcurrent2',
           'ompParallelDo', 'ompParallelLoop', 'ompTarget', 'ompTargetDataless',
           'ompTargetArraySyntax', 'ompWorkshare']
+
+linestyle_e = [(0, (5, 1)),'--','-.',':', # intrinsic
+               (0, (5, 1)),'--','-.',':', # OpenMP
+                   '--','-.']     # OpenMP Target
 
 
 file = 'official/output.txt'
@@ -80,32 +95,34 @@ for prob_name, problem in data.groupby('problem_enum'):
 
 
 
-def set_and_run_plot(group_by):
-    if (group_by == 'compiler_enum'):
-        enum = compiler_e
-
+def set_and_run_plot():
     for prob_num, prob_data in data.groupby("problem_enum"):
+        # if (prob_num == problem_c.matsum.value):
+        #     continue
         print("---",problem_e[prob_num],"---")
+        # return prob_data
 
         fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
         plt.suptitle(problem_e[prob_num], fontsize=14)
         plt.xscale("log")
         axes = plt.gca()
-        axes.set_ylim([0,1])
+        # axes.set_ylim([0,0.05])
+        axes.set_ylim([0,2])
 
-        for compiler, c_data in prob_data.groupby(group_by):
+        for compiler, c_data in prob_data.groupby("compiler_enum"):
             print("   ---",compiler_e[compiler],"---")
             i = int(compiler/2); j =  compiler%2
             axs[i,j].set_title(compiler_e[compiler])
 
             for method, m_data in c_data.groupby("method_enum"):
-            # q.plot(x=' n', y='time', kind='line', ax=axs[i,j], alpha=0.75,
+                print("      ---", name_e[method], method)
+
                 lw=2
-                ls=['-','--','-.',':'][method%4]
 
                 m_data.plot(x=' n', y='time', ax=axs[i,j], alpha=0.75,
-                            label=name_e[method], marker=getSymbol(compiler),
-                            color=color_map[method], linestyle=ls, linewidth=lw)
+                            label=name_e[method],
+                            color=color_map[method],
+                            linestyle=linestyle_e[method], linewidth=lw)
 
 
             # plt.show()
@@ -117,30 +134,6 @@ def set_and_run_plot(group_by):
 
 
 choices = ['method_enum', 'problem_enum', 'compiler_enum']
-q = set_and_run_plot(choices[2])
-
-
-print('Fin')
-sys.exit()
-
-
-for prob_name, problem in data.groupby('method_enum'):
-
-
-    fig, ax = plt.subplots()
-    ax.set_title(problem_e[prob_name])
-    for name, group in problem.groupby('method_enum'):
-        lw=5-4*name/len(color)
-
-        # print(lw)
-        lw=2
-        ls=['-','--','-.',':'][name%4]
-
-        group.plot(x=' n', y='time', kind='line', ax=ax, alpha=0.75,
-                   label=name_e[name], color=color_map[name], linestyle=ls,
-                   linewidth=lw, marker=getSymbol(name))
-
-
-plt.show()
+q = set_and_run_plot()
 
 print("FIN")
